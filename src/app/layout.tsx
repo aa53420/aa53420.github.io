d9cb31d436bcfbe81f90e7fa823ea3e5b95fc4d9
moo <aa53420@gmail.com>
2023-08-31 19:58:06 +0900

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Sidebar />
         {children}
       </body>
     </html>
