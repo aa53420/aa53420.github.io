b64946e4812802c153acb272dc874ed715e9e59d
moo <aa53420@gmail.com>
2023-08-31 11:29:08 +0900

    flowbite 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   const postData = getPostData(Category.GAME)
   return (
     <>
-
+      <div className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+        <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
+      </div>
     </>
   )
 }
