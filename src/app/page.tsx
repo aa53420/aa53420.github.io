73e8f676b2101274ed8a278a979f5503f09f2a28
moo <aa53420@gmail.com>
2023-08-31 19:50:08 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import {Category, getPostData} from "@/lib/Post";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   const postData = getPostData(Category.GAME)
   return (
     <>
-      <div className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-        <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-      </div>
+
     </>
   )
 }
